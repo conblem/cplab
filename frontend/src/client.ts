9ada8ed2deacd94ce539d06ca62b1f668520eb9e
conblem <mail@conblem.me>
2025-02-11 18:58:26 +0100

    fix application build

--- frontend/src/client.ts
+++ frontend/src/client.ts
@@ -2,6 +2,7 @@ import { initClient } from "@ts-rest/core";
 import { contract } from "@/app/api/rest/[...ts-rest]/contract";
 
 export default initClient(contract, {
-  baseUrl: `${location.protocol}//${location.host}`,
+  // baseUrl: `${location.protocol}//${location.host}`,
+  baseUrl: `https://${process.env.VERCEL_URL}`,
   baseHeaders: {},
 });
